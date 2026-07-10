57daa4c4f3b89db92c85758f5cb1837a046fe829
itsaschoolbus <91535129+itsaschoolbus@users.noreply.github.com>
2026-07-10 15:13:14 +0700

    ae

--- circle.sh
+++ circle.sh
@@ -2,8 +2,13 @@ repo init -u https://github.com/ProjectCiRCLE-ROM/manifest.git -b 16.2 --git-lfs
 /opt/crave/resync.sh
 
 source build/envsetup.sh
+export BUILD_USERNAME='いろは'
+export BUILD_HOSTNAME='月読'
 export CIRCLE_BUILDTYPE=OFFICIAL
 export WITH_GMS=true
 lunch circle_alioth-bp4a-userdebug
 m installclean
 m live
+git clone https://github.com/ProjectCiRCLE-ROM/OTA.git
+chmod +x OTA/createjson.sh
+. OTA/createjson.sh
