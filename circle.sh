1c27dcc95eb2ece577bc3646e2b2368d6cd39c16
itsaschoolbus <91535129+itsaschoolbus@users.noreply.github.com>
2026-08-11 12:04:22 +0700

    temp build test

--- circle.sh
+++ circle.sh
@@ -2,14 +2,8 @@ repo init -u https://github.com/ProjectCiRCLE-ROM/manifest.git -b 16.2 --git-lfs
 /opt/crave/resync.sh
 
 source build/envsetup.sh
-export BUILD_USERNAME='いろは'
-export BUILD_HOSTNAME='月読'
-export CIRCLE_BUILDTYPE=OFFICIAL
 export WITH_GMS=true
 lunch circle_alioth-bp4a-userdebug
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
 m installclean
-m live || exit 1
-git clone https://github.com/ProjectCiRCLE-ROM/OTA.git
-chmod +x OTA/createjson.sh
-. OTA/createjson.sh > ota.json
+m live
